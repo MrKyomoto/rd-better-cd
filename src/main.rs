1583ccb9b48e34148e3b359818aa2091dea58872
kyomoto <2028566723@qq.com>
2025-11-20 18:34:33 +0800

    src: update main

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ use crate::app::App;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut terminal = ratatui::init();
-    let mut app = App::new();
+    let mut app = App::new()?;
     let app_result = app.run(&mut terminal);
     ratatui::restore();
     let temp_path = "/tmp/rd_selected_dir";
     if app.output {
-        fs::write(temp_path, app.finnal_dir())?;
+        fs::write(temp_path, app.finnal_dir()?)?;
     } else {
         fs::write(temp_path, app.current_dir)?;
     }
